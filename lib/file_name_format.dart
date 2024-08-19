1ecc38c6f2bd6c8537a2ded6ae792fa5c81cf370
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2024-08-19 16:30:21 +0300

    task: update fileNameWithOutExtension to handle range error.

--- lib/file_name_format.dart
+++ lib/file_name_format.dart
@@ -47,21 +47,14 @@ class FileNameFormat {
       // Find the index of the last dot (before the file extension)
       int dotIndex = fileNameWithExtension.lastIndexOf('.');
 
-      // Find the index of the first query parameter (?)
-      // int queryParamIndex = fileNameWithExtension.indexOf('?');
-
-      // Extract the file extension
-      // String fileExtension;
-      // if (queryParamIndex != -1) {
-      //   fileExtension =
-      //       fileNameWithExtension.substring(dotIndex + 1, queryParamIndex);
-      // } else {
-      //   fileExtension = fileNameWithExtension.substring(dotIndex + 1);
-      // }
-
       // Extract the file name without extension
-      String nameWithoutExtension =
-          fileNameWithExtension.substring(0, dotIndex);
+      String nameWithoutExtension;
+      if (dotIndex != -1) {
+        nameWithoutExtension = fileNameWithExtension.substring(0, dotIndex);
+      } else {
+        // If there's no dot, use the whole filename
+        nameWithoutExtension = fileNameWithExtension;
+      }
 
       // Remove any prefix before the last slash
       int lastSlashInName = nameWithoutExtension.lastIndexOf('/');
@@ -69,14 +62,17 @@ class FileNameFormat {
         nameWithoutExtension =
             nameWithoutExtension.substring(lastSlashInName + 1);
       }
-      debugPrint('FUNCTION NAME $nameWithoutExtension');
+
+      debugPrint('fileNameWithOutExtension: $nameWithoutExtension');
       return nameWithoutExtension;
     } on Exception catch (e) {
       debugPrint('fileNameWithOutExtension:[ $url ]: E: $e');
       final randomNumber = Random().nextInt(1000);
       final now = DateTime.now();
-      final generatedFileName = 'file_${randomNumber}_${now}_';
-      debugPrint('fileNameWithOutExtension: E: generated file name: $generatedFileName');
+      final generatedFileName =
+          'file_${randomNumber}_${now.millisecondsSinceEpoch}';
+      debugPrint(
+          'fileNameWithOutExtension: E: generated file name: $generatedFileName');
       return generatedFileName;
     }
   }
